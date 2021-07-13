ead2ba62c448c84fc9097007a32b20c1df55a23e
melyman95 <mattlyman12@gmail.com>
2021-07-13 04:17:23 -0700

    Added getAge method

---
+++ SourceControlConsoleApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl
+{
+    class SourceControlConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            string getName() {
+                string name;
+                Console.Write("Hey! What's your name? ");
+                name = Console.ReadLine();
+
+                return name;
+            }
+
+            string getAge() {
+                string age;
+                Console.Write("How old are you? ");
+                age = Console.ReadLine();
+
+                return age;
+            }
+
+            string userName = getName();
+            Console.Write("Hello " + userName + ", that's a great name.");
+
+            string userAge = getAge();
+            if (Int32.Parse(userAge) >= 65) {
+                Console.Write(userAge + "?! Wow, you're old.");
+            }
+        }
+    }
+}
