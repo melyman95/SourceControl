6e269adcb00d6b8ffe610f504033a6988e140727
melyman95 <mattlyman12@gmail.com>
2021-07-13 04:06:22 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string getName() {
+                string name;
+                Console.Write("Hey! What's your name? ");
+                name = Console.ReadLine();
+
+                return name;
+            }
+        }
+    }
+}
